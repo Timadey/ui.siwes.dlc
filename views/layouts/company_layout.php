14b2777b7a117914d5e809b7c6525cb1cc8599de
Timothy Adeleke <dlktimothy@gmail.com>
2024-04-12 18:17:44 +0100

    Increase width of body section

--- views/layouts/company_layout.php
+++ views/layouts/company_layout.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.css" />
   <link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.css" />
   <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.css" />
-  <link rel="stylesheet" href="/siwes/dlc/assets/styles/profile.css">
+  <!-- <link rel="stylesheet" href="/siwes/dlc/assets/styles/profile.css"> -->
   <link rel="stylesheet" href="/siwes/dlc/assets/styles/my-datatable.css">
 
 </head>
